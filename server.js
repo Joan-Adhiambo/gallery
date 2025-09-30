2d556c91409089e44637ff684d2955ebffc175fb
Joan-Adhiambo <adhiambojoan83@gmail.com>
2025-09-30 23:33:32 +0300

    resolve mongoose connection

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 const config = require('./_config');
 
 // Define routes
-let index = require('./routes/index');
-let image = require('./routes/image');
+const index = require('./routes/index');
+const image = require('./routes/image');
 
-// Initializing the app
+// Initialize the app
 const app = express();
 
 // View Engine
@@ -17,8 +16,9 @@ app.set('view engine', 'ejs');
 // Set up the public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// body parser middleware
+// Body parser middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // 🚩 Dummy route if running tests
 if (process.env.NODE_ENV === 'test') {
@@ -34,7 +34,8 @@ if (process.env.NODE_ENV === 'test') {
 }
 
 // Connect to database and start server
-const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env];
+const MONGODB_URI =
+  process.env.MONGODB_URI || config.mongoURI[app.settings.env];
 const PORT = process.env.PORT || 5000;
 
 async function startServer() {
@@ -56,6 +57,7 @@ async function startServer() {
 startServer();
 
 module.exports = app;
+
 
 
 
